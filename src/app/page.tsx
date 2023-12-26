bd2a388bb7a36953cb202f1551de7be758001bf9
ovsbrian <ovsbrian@gmail.com>
2023-12-26 04:43:30 -0300

    fix separator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
+ 
 
-import { General } from '../components/nombre'
+ 
 
 export default function Home() {
   return (
         <>
         <div className='flex justify-center h-screen items-center'>
-
-         <General/>
+ 
         </div>
         </>
   )
