441e7cd769fbe92ae9951b545b0d0f93bb00e6fa
nicolasamoroso <108936163+nicolasamoroso@users.noreply.github.com>
2023-12-30 18:18:20 -0300

    add sortablejs, emojipicker and emojiitem components

---
+++ src/components/Emoji/EmojiPicker.tsx
@@ -0,0 +1,47 @@
+"use client"
+
+import { useState } from "react"
+import { ReactSortable } from "react-sortablejs"
+
+import Emoji from "@/types/emojisTypes"
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card"
+import EmojiItem from "./EmojiItem"
+
+const EMOJIS_STORAGE_KEY: string = "emojis"
+
+const EmojiPicker = ({ emojis }: { emojis: Emoji[] }) => {
+  const emojisFromStorage = JSON.parse(localStorage.getItem(EMOJIS_STORAGE_KEY) || "null")
+  const [state, setState] = useState<Emoji[]>(emojisFromStorage || emojis)
+
+  function updateEmojis(ev: any) {
+    const { oldIndex, newIndex } = ev
+    const items = [...state]
+    const itemToMove = items[oldIndex]
+    const itemToReplace = items[newIndex]
+
+    items[oldIndex] = itemToReplace
+    items[newIndex] = itemToMove
+
+    localStorage.setItem(EMOJIS_STORAGE_KEY, JSON.stringify(items))
+  }
+
+  return (
+    <HoverCard>
+      <HoverCardTrigger className="cursor-pointer">Hover</HoverCardTrigger>
+      <HoverCardContent className="overflow-auto h-[250px]">
+        <ReactSortable
+          list={state}
+          setList={setState}
+          onUpdate={(ev) => updateEmojis(ev)}
+          className="grid grid-cols-3 grid-rows-3 gap-2"
+        >
+          {state.map((item) => (
+            <EmojiItem key={item.id} emoji={item.emoji} />
+          ))}
+        </ReactSortable>
+      </HoverCardContent>
+    </HoverCard>
+  )
+}
+
+export default EmojiPicker
