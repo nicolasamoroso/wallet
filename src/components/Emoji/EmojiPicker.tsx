b74c36cef150f781d560bc8ccd4278fdb8eb02b5
nicolasamoroso <108936163+nicolasamoroso@users.noreply.github.com>
2024-01-03 16:36:12 -0300

    add popover ui

--- src/components/Emoji/EmojiPicker.tsx
+++ src/components/Emoji/EmojiPicker.tsx
@@ -3,31 +3,32 @@
 import { useState } from "react"
 import Picker, { EmojiClickData, SuggestionMode, Theme } from "emoji-picker-react"
 
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
+
 const EmojiPicker = () => {
   const onEmojiClick = (emojiData: EmojiClickData) => {
     console.log(emojiData)
   }
-
-  const [open, setOpen] = useState(false)
 
   let color = Theme.LIGHT
   let suggested = SuggestionMode.RECENT
   let preview = { showPreview: false }
   return (
     <>
-      <button onClick={() => setOpen((prev) => !prev)} className="bg-black text-white">
-        abrir
-      </button>
-      {open && (
-        <Picker
-          theme={color}
-          onEmojiClick={onEmojiClick}
-          autoFocusSearch={false}
-          previewConfig={preview}
-          suggestedEmojisMode={suggested}
-          className="select-none"
-        />
-      )}
+      <Popover>
+        <PopoverTrigger>Open</PopoverTrigger>
+        <PopoverContent className="md:max-w-[350px] sm:w-[280px] max-h-[450px]  p-0 rounded-xl">
+          <Picker
+            theme={color}
+            onEmojiClick={onEmojiClick}
+            autoFocusSearch={false}
+            previewConfig={preview}
+            suggestedEmojisMode={suggested}
+            className="select-none"
+            width={"100%"}
+          />
+        </PopoverContent>
+      </Popover>
     </>
   )
 }
