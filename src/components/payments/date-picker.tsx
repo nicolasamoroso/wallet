680cab60b0991e08468c1245cea84fcebc1c1cfb
nicolasamoroso <108936163+nicolasamoroso@users.noreply.github.com>
2024-07-08 00:39:54 -0300

    calendar on phones

--- src/components/payments/date-picker.tsx
+++ src/components/payments/date-picker.tsx
@@ -1,12 +1,13 @@
 "use client"
 
+import { useState } from "react"
 import { CalendarIcon } from "@radix-ui/react-icons"
 import { format } from "date-fns"
-import { ControllerRenderProps } from "react-hook-form"
+import { ControllerRenderProps, FieldValues } from "react-hook-form"
 
 import { Button } from "@/components/ui/button"
 import { Calendar } from "@/components/ui/calendar"
-import { FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { FormControl, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
 import { cn } from "@/utils/clsx"
 
@@ -24,27 +25,33 @@ const DatePicker = ({
     "dob"
   >
 }) => {
+  const [open, setOpen] = useState(false)
   return (
     <FormItem className="flex flex-col gap-2 col-span-6">
       <FormLabel htmlFor="date">Fecha</FormLabel>
-      <Popover>
+      <Popover onOpenChange={setOpen} open={open}>
         <PopoverTrigger asChild>
-          <Button
-            variant={"outline"}
-            className={cn(
-              "pl-3 text-left font-normal bg-input-background",
-              !field.value && "text-muted-foreground"
-            )}
-          >
-            {field.value ? format(field.value, "PPP") : <span>01/01/2024</span>}
-            <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-          </Button>
+          <FormControl>
+            <Button
+              variant={"outline"}
+              className={cn(
+                "pl-3 text-left font-normal bg-input-background",
+                !field.value && "text-muted-foreground"
+              )}
+            >
+              {field.value ? format(field.value, "PPP") : <span>01/01/2024</span>}
+              <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+            </Button>
+          </FormControl>
         </PopoverTrigger>
         <PopoverContent className="w-auto p-0" align="start">
           <Calendar
             mode="single"
             selected={field.value}
-            onSelect={field.onChange}
+            onSelect={() => {
+              setOpen(false)
+              field.onChange
+            }}
             disabled={(date) => date > new Date() || date < new Date("1900-01-01")}
             initialFocus
           />
